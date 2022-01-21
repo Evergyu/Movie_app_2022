86fe83b63febf09c00094d2b8812d5f7173f4201
Evergyu <iingy123@naver.com>
2022-01-21 14:48:11 +0900

    Organizing files and implementing functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food(props){
+  return (
+    <div>
+      <h1>I love {props.name} </h1>
+      <img src={props.image} alt={props.name}/>
+      </div>
+  ) 
+}
+
+const foodIlike=[
+  {name : 'kimchi', image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.K38fMEczTi_WllQaQXvDgAHaGp%26pid%3DApi&f=1'},
+  {name : 'ddeok-bbok-i', image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.kMNtwT_3Sp-W-yqYfPcRRAHaHa%26pid%3DApi&f=1'}
+
+] 
+
+function renderfood(dish){
+  return (<Food name={dish.name} image={dish.image}/>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodIlike.map(renderfood)}
     </div>
   );
 }
